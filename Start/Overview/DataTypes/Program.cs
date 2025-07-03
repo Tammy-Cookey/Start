2f6066e09a8a57f06003f995e3c2067366581d6a
Tammy-Cookey <tammy7cookey@gmail.com>
2025-07-03 10:53:07 +0100

    datatype

--- Start/Overview/DataTypes/Program.cs
+++ Start/Overview/DataTypes/Program.cs
@@ -13,9 +13,15 @@ Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
 
 // TODO: implicit type conversion
 Console.WriteLine($"{c+a}");
-Console.Writeline($"{(char)(c+a)}")
-// TODO: Create an instance of a struct (which is a value type)
+Console.WriteLine($"{(char)(c+a)}");
 
+
+Console.WriteLine($"{f + a}");
+Console.WriteLine($"{f + c}");  
+// TODO: Create an instance of a struct (which is a value type)
+s s1;
+s1.a= 5;
+s1.b = false;
 
 // Perform an operation on a struct
 void StructOp(s theStruct) {
@@ -25,13 +31,18 @@ void StructOp(s theStruct) {
     Console.WriteLine($"{theStruct.a}, {theStruct.b}");
 }
 
+Console.WriteLine("Structs are passed by copy, since they are value types:");
+Console.WriteLine($"{s1.a}, {s1.b}");
+StructOp(s1);  
+Console.WriteLine($"{s1.a}, {s1.b}");
+
 // Console.WriteLine("Structs are passed by copy, since they are value types:");
 // Console.WriteLine($"{s1.a}, {s1.b}");
 // StructOp(s1);
 // Console.WriteLine($"{s1.a}, {s1.b}");
 
 // TODO: Create an object instance of a class (which is a reference type)
-
+MyClass cl = new MyClass{a=5, b=false};
 
 // Perform an operation on the class
 void ClassOp(MyClass theClass) {
@@ -40,6 +51,11 @@ void ClassOp(MyClass theClass) {
     theClass.b = true;
     Console.WriteLine($"{theClass.a}, {theClass.b}");
 }
+
+Console.WriteLine("Objects are passed by reference, since they are reference types:");
+Console.WriteLine($"{cl.a}, {cl.b}");   
+ClassOp(cl);
+Console.WriteLine($"{cl.a}, {cl.b}");
 
 // Console.WriteLine("Objects are passed by reference, since they are reference types:");
 // Console.WriteLine($"{cl.a}, {cl.b}");
