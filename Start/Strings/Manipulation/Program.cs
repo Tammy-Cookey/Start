de6b72d7cae98402f63536be6ce68c96c02a2620
Tammy-Cookey <tammy7cookey@gmail.com>
2025-07-05 12:05:18 +0100

    Manipulation

--- Start/Strings/Manipulation/Program.cs
+++ Start/Strings/Manipulation/Program.cs
@@ -7,26 +7,35 @@ string str3 = "THIS is a STRING";
 string[] strs = {"one", "two", "three", "four"};
 
 // TODO: Length of a string 
-
+Console.WriteLine(str1.Length); // Outputs the number of characters in str1
 
 // TODO: Access individual characters
-
+Console.WriteLine(str1[14]);
 
 // TODO: Iterate over a string like any other sequence of values
-
-
+foreach (Char ch in str1) {
+    Console.Write(ch);
+    if (ch == 'b') {
+        Console.WriteLine();
+        break;
+    }
+}
 // TODO: String Concatenation         
 string outstr;
-
+outstr = String.Concat(strs);
+Console.WriteLine(outstr); // Outputs: one two three four
 
 // TODO: Joining strings together with Join
-
+outstr = String.Join(", ", strs);
+Console.WriteLine(outstr); // Outputs: one, two, three, four
+outstr = String.Join(" --- ", strs);
+Console.WriteLine(outstr); // Outputs: one --- two --- three --- four
 
 // TODO: String Comparison
 
 // Equals just returns a regular Boolean
-// bool isEqual = str2.Equals(str3);
-// Console.WriteLine($"{isEqual}");
+bool isEqual = str2.Equals(str3);
+ Console.WriteLine($"{isEqual}");
 
 // Compare will perform an ordinal comparison and return:
 // < 0 : first string comes before second in sort order
@@ -35,3 +44,5 @@ string outstr;
 
 
 // TODO: Replacing content
+outstr = str1.Replace("fox", "cat");
+Console.WriteLine($"{outstr}");
