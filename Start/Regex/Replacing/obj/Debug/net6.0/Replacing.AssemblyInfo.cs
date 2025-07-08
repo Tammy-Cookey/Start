3958cd9bf3eacd5767453d08ae3d88390ad643ef
Tammy-Cookey <tammy7cookey@gmail.com>
2025-07-08 18:30:25 +0100

    Regex Replacing

--- Start/Regex/Replacing/obj/Debug/net6.0/Replacing.AssemblyInfo.cs
+++ Start/Regex/Replacing/obj/Debug/net6.0/Replacing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Replacing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd267fe672251b9a3c1fc24ea9145c855928ec9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01e02cb50cfd0e297a7c76cf2dfb9f6ee138474")]
 [assembly: System.Reflection.AssemblyProductAttribute("Replacing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Replacing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
